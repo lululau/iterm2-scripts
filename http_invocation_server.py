906873e88ad275450eafd847b0182488b6ec36b4
Liu Xiang <liuxiang@ktjr.com>
2019-09-29 12:32:54 +0800

    Auto kill previous http_invocation_server process when iTerm2 starts

--- http_invocation_server.py
+++ http_invocation_server.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3.7
 
+import pathlib
+import os
+import signal
 import asyncio
 import iterm2
 import json
 from http.server import BaseHTTPRequestHandler,HTTPServer
+
+PID_FILE = str(pathlib.Path("~/Library/ApplicationSupport/iTerm2/http_invocation_server.pid").expanduser())
 
 PORT_NUMBER = 28082
 
@@ -16,6 +21,26 @@ async def invoke(invocation):
     if connection == None:
         connection = await iterm2.Connection.async_create()
     return await iterm2.async_invoke_function(connection, invocation)
+
+def save_pid():
+    with open(PID_FILE, 'w') as f:
+        f.write(str(os.getpid()))
+
+def get_previous_pid():
+    if os.path.exists(PID_FILE):
+        with open(PID_FILE) as f:
+            pid_str = f.readline()
+            return int(pid_str)
+    else:
+        return None
+
+def kill_previous_process():
+    pid = get_previous_pid()
+    if pid != None:
+        os.kill(pid, signal.SIGKILL)
+
+kill_previous_process()
+save_pid()
 
 class InvocationHandler(BaseHTTPRequestHandler):
 
